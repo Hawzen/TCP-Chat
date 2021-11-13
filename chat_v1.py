aeeba29ccafb3ba1cfbc6c156fe7dfe6fc9a34e7
Hawzen <hawzen@users.noreply.github.com>
2021-11-13 20:20:42 +0300

    Fix message_first reversing bug

--- chat_v1.py
+++ chat_v1.py
@@ -23,16 +23,14 @@ def message_remote(remote_socket: socket.socket, message_first: bool) -> None:
     if message_first: 
         while True:
             print(f"\nMessage number {message_count}")
-            process_remote_message(remote_socket, ip=remote_ip, port=remote_port)
             make_and_send_local_message(remote_socket)
-
+            process_remote_message(remote_socket, ip=remote_ip, port=remote_port)
             message_count += 1
     else:
         while True:
             print(f"\nMessage number {message_count}")
-            make_and_send_local_message(remote_socket)
             process_remote_message(remote_socket, ip=remote_ip, port=remote_port)
-
+            make_and_send_local_message(remote_socket)
             message_count += 1
 
 def process_remote_message(remote_socket: socket.socket, ip: str, port: int) -> None:
